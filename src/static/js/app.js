8494735370c1a975a4bd71e0f37ee7cd051f9ad3
Molina121426 <diegomolina.121426@gmail.com>
2024-10-24 15:22:03 -0500

    se modifica texto

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">Listas de tareas vacias, agrega uno arriba sapahijueputa!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
